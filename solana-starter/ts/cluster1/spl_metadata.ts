bc5657aa55ccf69432ad9436175b51dcc66f0c2a
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-07-04 11:57:42 +0000

    added spl minting code

--- solana-starter/ts/cluster1/spl_metadata.ts
+++ solana-starter/ts/cluster1/spl_metadata.ts
@@ -1,8 +1,8 @@
 import wallet from "../turbin3-wallet.json"
 import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
+import {
+    createMetadataAccountV3,
+    CreateMetadataAccountV3InstructionAccounts,
     CreateMetadataAccountV3InstructionArgs,
     DataV2Args
 } from "@metaplex-foundation/mpl-token-metadata";
@@ -10,7 +10,7 @@ import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-fo
 import { bs58 } from "@coral-xyz/anchor/dist/cjs/utils/bytes";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("AQLBNURCQUn89LKkqTLCAUNXi51Pv3xhyVLHH3j2v5AH")
 
 // Create a UMI connection
 const umi = createUmi('https://api.devnet.solana.com');
@@ -21,29 +21,38 @@ umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 (async () => {
     try {
         // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+        let accounts: CreateMetadataAccountV3InstructionAccounts = {
+            mint,
+            mintAuthority: signer
+        }
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
+        let data: DataV2Args = {
+            name: "lxsh",
+            symbol: "LX",
+            collection: null,
+            creators: null,
+            uses: null,
+            sellerFeeBasisPoints: 100,
+            uri: ""
+        }
 
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
+        let args: CreateMetadataAccountV3InstructionArgs = {
+            data,
+            isMutable: false,
+            collectionDetails: null,
+        }
 
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
+        let tx = createMetadataAccountV3(
+            umi,
+            {
+                ...accounts,
+                ...args
+            }
+        )
 
-        // let result = await tx.sendAndConfirm(umi);
-        // console.log(bs58.encode(result.signature));
-    } catch(e) {
+        let result = await tx.sendAndConfirm(umi);
+        console.log("successfully created metadata : ",bs58.encode(result.signature));
+    } catch (e) {
         console.error(`Oops, something went wrong: ${e}`)
     }
 })();
--- solana-starter/ts/cluster1/spl_mint.ts
+++ solana-starter/ts/cluster1/spl_mint.ts
@@ -1,6 +1,7 @@
 import { Keypair, PublicKey, Connection, Commitment } from "@solana/web3.js";
-import { getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
+import { createAssociatedTokenAccount, getOrCreateAssociatedTokenAccount, mintTo } from '@solana/spl-token';
 import wallet from "../turbin3-wallet.json"
+import { publicKey } from "@metaplex-foundation/umi";
 
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
@@ -12,18 +13,33 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 const token_decimals = 1_000_000n;
 
 // Mint address
-const mint = new PublicKey("<mint address>");
+const mint = new PublicKey("AQLBNURCQUn89LKkqTLCAUNXi51Pv3xhyVLHH3j2v5AH");
 
-(async () => {
-    try {
-        // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
 
-        // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
-    } catch(error) {
-        console.log(`Oops, something went wrong: ${error}`)
-    }
-})()
+    (async () => {
+        try {
+            // Create an ATA
+            const ata = await getOrCreateAssociatedTokenAccount(
+                connection,
+                keypair,
+                mint,
+                keypair.publicKey
+            )
+            console.log(`Your ata is: ${ata.address.toBase58()}`);
+
+
+            // Mint to ATA
+            const mintTx = await mintTo(
+                connection,
+                keypair,
+                mint,
+                ata.address,
+                keypair.publicKey,
+                100n * token_decimals
+            )
+
+            console.log(` ✅ Your mint txid: ${mintTx}`);
+        } catch (error) {
+            console.log(`Oops, something went wrong: ${error}`)
+        }
+    })()
