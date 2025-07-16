2156082cf797b0cc1aaefcfda8b980b819136213
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-07-16 21:06:21 +0000

    defined context for claim ixn

--- nft-staking/programs/nft-staking/src/instructions/claim.rs
+++ nft-staking/programs/nft-staking/src/instructions/claim.rs
@@ -1,1 +1,38 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{ associated_token::AssociatedToken, token::{ Mint, Token, TokenAccount } };
+
+use crate::{ StakeConfig, UserAccount };
+
+#[derive(Accounts)]
+pub struct Claim<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"user".as_ref(),user.key().as_ref()],
+        bump = user_account.bump
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(
+        mut,
+        seeds = [b"rewards".as_ref(),config.key().as_ref()],
+        bump = config.rewards_bump
+    )]
+    pub rewards_mint: Account<'info, Mint>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        associated_token::mint = rewards_mint,
+        associated_token::authority = user
+    )]
+    pub rewards_ata: Account<'info, TokenAccount>,
+    #[account(seeds = [b"config".as_ref()], bump = config.bump)]
+    pub config: Account<'info, StakeConfig>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+}
