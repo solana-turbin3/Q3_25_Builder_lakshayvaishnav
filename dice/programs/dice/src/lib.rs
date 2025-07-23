160041d8aa1852381aa485bfa8b1050503b452b3
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-07-24 02:46:39 +0530

    updated lib.rs file

--- dice/programs/dice/src/lib.rs
+++ dice/programs/dice/src/lib.rs
@@ -15,7 +15,21 @@ declare_id!("7phWwNyQ3givYQLULkLtjV9mPqPFWLJjqSBor8XaVHYA");
 pub mod dice {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+    pub fn initialize(ctx: Context<Initialize>, amount: u64) -> Result<()> {
+        ctx.accounts.init(amount)
+    }
+
+    pub fn place_bet(ctx: Context<PlaceBet>, seed: u128, roll: u8, amount: u64) -> Result<()> {
+        ctx.accounts.create_bet(&ctx.bumps, seed, roll, amount)?;
+        ctx.accounts.deposit(amount)
+    }
+
+    pub fn resolve_bet(ctx: Context<ResolveBet>, sig: Vec<u8>) -> Result<()> {
+        ctx.accounts.verify_ed25519_signature(&sig)?;
+        ctx.accounts.resolve_bet(&ctx.bumps, &sig)
+    }
+
+    pub fn refund_bed(ctx: Context<RefundBet>) -> Result<()> {
+        ctx.accounts.refund_bet(&ctx.bumps)
     }
 }
