19b140530cb825bb73bde8b484fbe7452d50c5f6
laksayvaishnav <thelakshayvaishnav@gmail.com>
2025-07-29 15:49:20 +0530

    added contexts and instructions

--- anchor-counter-er/programs/anchor-counter-er/src/lib.rs
+++ anchor-counter-er/programs/anchor-counter-er/src/lib.rs
@@ -7,6 +7,9 @@ pub mod instructions;
 pub mod state;
 
 use anchor_lang::prelude::*;
+use ephemeral_rollups_sdk::anchor::{ commit, delegate, ephemeral };
+use ephemeral_rollups_sdk::cpi::DelegateConfig;
+use ephemeral_rollups_sdk::ephem::{ commit_accounts, commit_and_undelegate_accounts };
 
 pub use constants::*;
 pub use instructions::*;
@@ -14,11 +17,120 @@ pub use state::*;
 
 declare_id!("G81C2dtvukGrr7qRgFZ5A11BqVnxN6VpeCJDxhrgXcAG");
 
+pub const TEST_PDA_SEED: &[u8] = b"test-pda";
+
+#[ephemeral]
 #[program]
 pub mod anchor_counter_er {
+    use ephemeral_rollups_sdk::anchor::ephemeral;
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        initialize::handler(ctx)
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
     }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+
+        counter.count += 1;
+
+        if counter.count > 10000 {
+            counter.count = 0;
+        }
+
+        Ok(())
+    }
+
+    pub fn delegate(ctx: Context<DelegateCounter>) -> Result<()> {
+        ctx.accounts.delegate_counter(
+            &ctx.accounts.payer,
+            &[TEST_PDA_SEED],
+            DelegateConfig::default()
+        )?;
+        Ok(())
+    }
+
+    pub fn commit(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        commit_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program
+        )?;
+
+        Ok(())
+    }
+
+    pub fn increment_and_commit(ctx: Context<IncrementAndCommit>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+
+        counter.count += 1;
+
+        commit_accounts(
+            &ctx.accounts.payer,
+            vec![&ctx.accounts.counter.to_account_info()],
+            &ctx.accounts.magic_context,
+            &ctx.accounts.magic_program
+        )?;
+
+        Ok(())
+    }
+}
+
+#[delegate]
+#[derive(Accounts)]
+pub struct DelegateCounter<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [TEST_PDA_SEED],
+        bump,
+        del
+    )]
+    pub counter: Account<'info, Counter>,
+}
+
+#[commit]
+#[derive(Accounts)]
+pub struct IncrementAndCommit<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [TEST_PDA_SEED],
+        bump,
+    )]
+    pub counter: Account<'info, Counter>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(init_if_needed, payer = user, space = 8 + 8, seeds = [TEST_PDA_SEED], bump)]
+    pub counter: Account<'info, Counter>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(
+        mut,
+        seeds = [TEST_PDA_SEED],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
 }
